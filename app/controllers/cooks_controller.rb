c8ce8f72c28f2958b3975a5ab3f0e94e6d069784
kosuke-kurono <kurono.nexus6@gmail.com>
2022-11-07 23:41:42 +0000

    Cookモデル作成

---
+++ app/controllers/cooks_controller.rb
@@ -0,0 +1,14 @@
+class CooksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @cook = Cook.new
+  end
+
+  def edit
+  end
+end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'cee65d381cb9ab9161d671ec956c3bd23e74b33d20fd4bd2bb1421ef60dcb63ee36760b01bd85a7eb85d6250133f5161b1ce7d9976e69edc0c7883c8d0dc6d37'
