c311bba5fb382d7835ae08eaab900d55e802055e
kosuke-kurono <kurono.nexus6@gmail.com>
2022-11-08 23:26:53 +0000

    レシピ詳細画面作成

--- app/controllers/cooks_controller.rb
+++ app/controllers/cooks_controller.rb
@@ -3,12 +3,22 @@ class CooksController < ApplicationController
   end
 
   def show
+    @cook = Cook.find(params[:id])
   end
 
   def new
     @cook = Cook.new
   end
-
+  def create
+    @cook = Cook.new(cook_params)
+    @cook.user_id =current_user.id
+    @cook.save
+    redirect_to cook_path(@cook)
+  end
   def edit
+  end
+  private
+  def cook_params
+    params.require(:cook).permit(:title,:body,:image)
   end
 end
